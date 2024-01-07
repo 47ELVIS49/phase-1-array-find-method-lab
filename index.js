2c241eee2ad017db9fd0150eada4a883a7829b95
47ELVIS49 <elvismomanyi23@.com>
2024-01-07 03:00:27 +0300

    done with array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+// superbowl.js
+
+// Your actual implementation of superbowlWin function
+function superbowlWin(record) {
+    const winningYearObj = record.find(entry => entry.result === 'W');
+    return winningYearObj ? winningYearObj.year : undefined;
+  }
+  
+  // Export the function for testing
+  module.exports = { superbowlWin };
+  
